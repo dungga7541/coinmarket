dbc9c6efe6e070c0b816fac545bed52750a9a5fd
NuocDua <dungga7541000@gmail.com>
2021-09-22 16:35:53 +0700

    first base

--- src/components/Homepage/HomePage.js
+++ src/components/Homepage/HomePage.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class componentName extends Component {
   render () {
     return (
       <div />
+
     )
   }
 }
